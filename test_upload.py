035f53ebca49b240d8ec785edb59a335961a8bb9
leoKV <kevorvs@gmail.com>
2025-08-05 13:26:13 -0600

    remove_test_upload

--- test_upload.py
+++
@@ -1,6 +0,0 @@
-from karafun_manager.utils.drive_manager import upload_file_to_folder
-
-file_path = "C:/karafun_service/songs-files/KUFYJ/kara_fun.kfn"
-folder_id = "1hh3LkXvKp4rrqmt063NmTU7kuy2z7ze0"  
-# Subir el archivo
-upload_file_to_folder(file_path=file_path, folder_id=folder_id)
